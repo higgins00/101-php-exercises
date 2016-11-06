dbd078427955d11e397db4374ab8cddc0a9aedcf
Higgins <danie.higgins@gmail.com>
2016-11-06 00:59:42 -0500

    guest list complete - returns correct number and prints out list

--- 01-guest-list.php
+++ 01-guest-list.php
@@ -1,11 +1,22 @@
 <?php
 	function returnRsvps($array) {
 		// Your function will be written here.
-	}
+}
 
 	// Fill in the array below with the names of your guests:
-	$guests = array();
+	$guests = array(
+		"Alex", 
+		"Nikki", 
+		"Sumeet",
+	);
+
+	$returnRsvps = count($guests);
+
+	$lastPerson = array_pop($guests); // c
+	$rsvpNames = implode(", ", $guests); // a, b
+	$rsvpNames .= " and ".$lastPerson; // a, b and c
+
 
 	// Call the function, passing in the $guests array:
-	echo returnRsvps($guests);
+	 echo "Your event has " . $returnRsvps . " RSVPs: " . $rsvpNames . ".";
 ?>
